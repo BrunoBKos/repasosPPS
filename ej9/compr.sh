69bbc2f73e35492619394d77cf16db5d9111417f
Bruno <brunoburkos@gmail.com>
2024-06-29 11:14:32 +0200

    te paso el bash

--- ej9/compr.sh
+++ ej9/compr.sh
@@ -1,3 +1,26 @@
 #!/usr/bin/bash
+function programa {
+if [ -e "main.c" ]; then
+    if [ -e "compr.c" ]; then
+        gcc -Wall -Wextra -Werror -ansi -pedantic main.c compr.c -o programa
+        exit 0
+    else 
+        exit 1
+    fi
+else
+    exit 1
+fi
+}
+function limpia {
+    if [ -e "programa" ]; then
+        rm ./programa
+        exit 0
+    fi
+        exit 1
+}
 
-LA FUNCION PETA
+if [ $1 = "limpia" ]; then
+    limpia
+elif [ $1 = "programa" ]; then
+    programa
+fi
